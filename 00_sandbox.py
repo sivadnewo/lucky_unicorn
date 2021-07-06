18eeee05616fa7c1215af7c8d5bc648fddc725b4
Owen Davis <davis.owen@awatapu.school.nz>
2021-07-06 13:03:47 +1200

    Initial commit

---
+++ 00_sandbox.py
@@ -0,0 +1,8 @@
+# Asks user for a number
+get_number = int(input("Choose a number?"))
+
+# Multiply the number by 5
+times_five = get_number * 5
+
+# Output the result
+print("{} times five is equal to {}".format (get_number))
