84dea218e1870cb8dd657214b9f011d614918514
sivadnewo <davis.owen@awatapu.school.nz>
2021-07-09 09:32:29 +1200

    Times by 5

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,8 +1,11 @@
-# Asks user for a number
+# Asks users for a number
 get_number = int(input("Choose a number?"))
 
 # Multiply the number by 5
 times_five = get_number * 5
 
+answer = "{} times five is equal to " \
+         "{}".format(get_number, times_five)
+
 # Output the result
-print("{} times five is equal to {}".format (get_number))
+print(answer)
