203305deb0afa52c226992ecaf61b0ba8144405b
sivadnewo <davis.owen@awatapu.school.nz>
2021-08-06 09:16:07 +1200

    Yes no check using if else

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -1,16 +1,19 @@
+
+show_instructions = ""
+while show_instructions.lower() != "xxx":
 # Ask the user if they have played before
 show_instructions = input("Have you played this game before?").lower()
 
 
-# If they say yes, output 'program continues'
-if show_instructions == "yes":
-    print("program continues")
-elif show_instructions == "y":
-    print("program continues")
-# If they say no, output 'display instructions'
-elif show_instructions == "no":
-    print("display instructions")
-elif show_instructions == "n":
-    print("display instructions")
-else:
-    print("please type yes or no")
+    # If they say yes, output 'program continues'
+    if show_instructions == "yes":
+        print("program continues")
+    elif show_instructions == "y":
+        print("program continues")
+    # If they say no, output 'display instructions'
+    elif show_instructions == "no":
+        print("display instructions")
+    elif show_instructions == "n":
+        print("display instructions")
+    else:
+        print("please type yes or no")
